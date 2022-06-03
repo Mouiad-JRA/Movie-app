fec8d514a129220b4b2f9105b8ef8437b321385c
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-03 13:29:05 +0300

    WIP :
    - add list view and detail view
    - add the template (need to spilt them)

--- src/movie/urls.py
+++ src/movie/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+
+from .views import MovieDetailView, MovieListView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('movies/', include('movie.urls')),
+    path('list', MovieListView.as_view(), name='movie-list'),
+    path('detail:<int:pk>', MovieDetailView.as_view(), name='movie-detail'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/movie/views.py
+++ src/movie/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.views.generic import ListView, DetailView
+
+from .models import Movie
+
+
+class MovieListView(ListView):
+    model = Movie
+
+
+class MovieDetailView(DetailView):
+    model = Movie
