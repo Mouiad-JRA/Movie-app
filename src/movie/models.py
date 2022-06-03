a1eeac7f3cf1f858fc89702ebc2140031734bf57
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-03 21:24:13 +0300

    WIP : - add MovieLinks

--- src/movie/models.py
+++ src/movie/models.py
@@ -27,8 +27,23 @@ class Movie(models.Model):
     category = models.CharField(max_length=8, choices=CATEGORY_CHOICES, default='Action')
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='EN')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='R')
+    cast = models.CharField(max_length=255)
     year_of_production = models.DateField()
     view_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+
+class MovieLinks(models.Model):
+    Link_CHOISES = (
+        ('D', 'DOWNLOAD LINK'),
+        ('W', 'WATCH LINK'),
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='movie_watch_link')
+    link = models.URLField()
+    type = models.CharField(choices=Link_CHOISES, max_length=1, default='D')
+
+    def __str__(self):
+        return self.movie.title
