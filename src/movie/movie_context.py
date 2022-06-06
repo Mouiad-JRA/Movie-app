310bb42f02cc2fbde89921701505cf26b99b44bf
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-06 21:25:44 +0300

    refactor: fix the templates.

--- src/movie/movie_context.py
+++ src/movie/movie_context.py
@@ -2,5 +2,5 @@ from .models import Movie
 
 
 def slider_movies(request):
-    movies = Movie.objects.all().order_by('created')[0:3]
+    movies = Movie.objects.all().order_by('created')[0:10]
     return {'slider_movies': movies}
